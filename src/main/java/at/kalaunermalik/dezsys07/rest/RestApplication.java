734ccec286b71349e6bf210775aa0bd0cb6cdac7
Paul Kalauner <pkalauner@student.tgm.ac.at>
2015-12-08 17:12:21 +0100

    Initial commit

---
+++ src/main/java/at/kalaunermalik/dezsys07/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package at.kalaunermalik.dezsys07.rest;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RestApplication {
+
+}
---
+++ src/main/java/at/kalaunermalik/dezsys07/soap/SoapApplication.java
@@ -0,0 +1,10 @@
+package at.kalaunermalik.dezsys07.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapApplication {
+
+
+}
