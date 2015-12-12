e27ecfaf2a1a86d0490f74833d4f32cce9f0ebb9
Paul Kalauner <pkalauner@student.tgm.ac.at>
2015-12-12 08:58:46 +0100

    update function

--- src/main/java/at/kalaunermalik/dezsys07/webinterface/WebController.java
+++ src/main/java/at/kalaunermalik/dezsys07/webinterface/WebController.java
@@ -2,7 +2,6 @@ package at.kalaunermalik.dezsys07.webinterface;
 
 
 import at.kalaunermalik.dezsys07.db.Entry;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,13 +9,12 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Controller
 public class WebController {
     private static final String POST_URL = "http://localhost:8080/entries";
-    private static final String DELETE_URL = "http://localhost:8080/entries/{id}";
+    private static final String GET_ENTRY = "http://localhost:8080/entries/{id}";
     private static final String GET_BY_TITLE = "http://localhost:8080/entries/title={title}";
     private final RestTemplate restTemplate;
 
@@ -46,23 +44,51 @@ public class WebController {
 
     @RequestMapping(value = "/update", method = RequestMethod.GET)
     public String updateGet() {
-        return "index";
+        return "update";
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public String updatePost(Model model) {
-    /*
-        Map<String, String> params = new HashMap<String, String>();
-        params.put("id", "2");
-
-        EmployeeVO updatedEmployee = new EmployeeVO(2, "New Name", "Gilly", "test@email.com");
-
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.put(uri, updatedEmployee, params);
-        */
-        return "result";
+    public String updatePost(@RequestParam(value = "entryid", required = false, defaultValue = "") String id, @RequestParam(value = "title", required = false, defaultValue = "") String title, Model model) {
+        Map<String, String> params = new HashMap<>();
+        model.addAttribute("entry", new Entry());
+        if (!id.isEmpty()) {
+            return "redirect:updateSubmit?id=" + id;
+        } else if (!title.isEmpty()) {
+            params.put("title", title);
+            Entry[] entries = restTemplate.getForObject(GET_BY_TITLE, Entry[].class, params);
+            if (entries.length > 0) {
+                return "redirect:updateSubmit?id=" + entries[0].getId();
+            }
+        }
+        model.addAttribute("resultmessage", "Es existiert kein Eintrag mit diesen Angaben!");
+        return "result_special";
     }
 
+    @RequestMapping(value = "/updateSubmit", method = RequestMethod.GET)
+    public String updateSubmitGet(@RequestParam(value = "id", required = true) String id, Model model) {
+        Map<String, String> params = new HashMap<>();
+        params.put("id", id);
+        Entry e = restTemplate.getForObject(GET_ENTRY, Entry.class, params);
+        if (e != null) {
+            model.addAttribute("entry", e);
+            return "updateSubmit";
+        }
+        model.addAttribute("resultmessage", "Es existiert kein Eintrag mit diesen Angaben!");
+        return "result_special";
+    }
+
+
+    @RequestMapping(value = "/updateSubmit", method = RequestMethod.POST)
+    public String updateSubmitPost(@ModelAttribute Entry entry, Model model) {
+        Map<String, String> params = new HashMap<>();
+        params.put("id", String.valueOf(entry.getId()));
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.put(GET_ENTRY, entry, params);
+        model.addAttribute("resultmessage", "Eintrag erfolgreich aktualisiert!");
+        model.addAttribute("entry", entry);
+        return "result";
+    }
 
     @RequestMapping(value = "/delete", method = RequestMethod.GET)
     public String deleteGet(Model model) {
@@ -75,7 +101,7 @@ public class WebController {
         Map<String, String> params = new HashMap<>();
         if (!id.isEmpty()) {
             params.put("id", id);
-            restTemplate.delete(DELETE_URL, params);
+            restTemplate.delete(GET_ENTRY, params);
 
             model.addAttribute("resultmessage", "Eintrag mit id " + id + " erfolgreich entfernt!");
         } else if (!title.isEmpty()) {
@@ -85,21 +111,21 @@ public class WebController {
             for (Entry cur : entries) {
                 params.clear();
                 params.put("id", String.valueOf(cur.getId()));
-                restTemplate.delete(DELETE_URL, params);
+                restTemplate.delete(GET_ENTRY, params);
                 sb.append(cur.getId()).append(", ");
             }
 
             if (entries.length == 0) {
                 model.addAttribute("resultmessage", "Keine Eintraege entfernt!");
             } else if (entries.length == 1) {
-                model.addAttribute("resultmessage", "Eintrag mit id " + entries[0] + " erfolgreich entfernt!");
+                model.addAttribute("resultmessage", "Eintrag mit id " + entries[0].getId() + " erfolgreich entfernt!");
             } else {
                 model.addAttribute("resultmessage", "Eintraege mit ids " + sb.toString() + " erfolgreich entfernt!");
             }
         } else {
             model.addAttribute("resultmessage", "Weder ID noch Titel angegeben!");
         }
-        return "result_delete";
+        return "result_special";
     }
 
     @ExceptionHandler(HttpClientErrorException.class)
